c690b6b0d2f3eee06d414994789ce718cd9ea814
siddhantkushwaha <k16.siddhant@gmail.com>
2024-09-06 14:48:38 +0530

    -

--- ollama_lib.py
+++ ollama_lib.py
@@ -1,5 +1,6 @@
 import json
 import os
+import time
 
 import ollama
 
@@ -16,17 +17,19 @@ def get_predefined_labels():
     return labels
 
 
-def generate_prompt(labels, subject, snippet, email_content):
+def generate_prompt(labels,sender, subject, snippet, email_content):
     with open(os.path.join(params.data_dir, 'prompt.txt'), 'r') as fp:
         content = fp.read()
     prompt = content.strip()
 
     labels = '\n'.join(labels)
+    sender = util.clean_sender(sender)
     subject = util.clean_text(subject)
     snippet = util.clean_text(snippet)
     email_content = util.clean_text(email_content)
 
     prompt = prompt.replace('<labels></labels>', f'<labels>{labels}</labels>')
+    prompt = prompt.replace('<sender></sender>', f'<sender>{sender}</sender>')
     prompt = prompt.replace('<subject></subject>', f'<subject>{subject}</subject>')
     prompt = prompt.replace('<snippet></snippet>', f'<snippet>{snippet}</snippet>')
     prompt = prompt.replace('<content></content>', f'<content>{email_content}</content>')
@@ -48,7 +51,7 @@ def parse_out(text, predefined_labels):
             print(f'LLM output not in correct format. [{text}], [{type(out_list)}]')
         else:
             for label in out_list:
-                if label in predefined_labels:
+                if label in predefined_labels or label == 'none':
                     llm_labels.append(label)
                 else:
                     print(f'LLM returned label [{label}] but it is not part of predefined list.')
@@ -70,16 +73,27 @@ def process_dump():
         predefined_labels = get_predefined_labels()
         prompt = generate_prompt(
             labels=predefined_labels,
+            sender=mail['Sender'],
             subject=mail['Subject'],
             snippet=mail['Text'],
-            email_content=mail['Text']
+            # email_content=mail['Text']
+            email_content=''
         )
 
-        text_response = ollama.generate(prompt=prompt, model=MODEL_NAME)['response']
+        wait_time = 2
 
-        out_list = parse_out(text_response, predefined_labels)
+        max_attempts = 5
+        for attempt in range(max_attempts):
+            text_response = ollama.generate(prompt=prompt, model=MODEL_NAME)['response']
+            out_list = parse_out(text_response, predefined_labels)
+            if len(out_list) > 0:
+                print(f"[{mail['Subject']}] -> [{', '.join(out_list)}]")
+                break
+            elif attempt == max_attempts - 1:
+                print(f'******************* Failed after {max_attempts} attempts ****************')
+            time.sleep(wait_time)
 
-        print(f"[{mail['Subject']}] -> [{', '.join(out_list)}]")
+        time.sleep(wait_time)
 
 
 if __name__ == '__main__':
