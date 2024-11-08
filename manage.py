4af327cd520dca1400cb062b3a981fe77d17e3a7
Siddhant Kushwaha <siddhant.kushwaha@cohesity.com>
2024-11-08 22:39:42 +0530

    Making manage.py user friendly.

--- manage.py
+++ manage.py
@@ -5,12 +5,18 @@ import rules_util
 
 def main():
     arg_to_func = {
-        '--dump_rules': rules_util.dump_rules,
-        '--update_rules': rules_util.update_rules_from_csv,
+        '--dump_rules': (rules_util.dump_rules, 'Dumps rules from sqlite db to rules.csv.'),
+        '--update_rules': (rules_util.update_rules_from_csv, 'Pushes rules from rules.csv to sqlite db.'),
     }
 
+    if len(sys.argv) < 2:
+        string_items = [f'{arg} - {arg_to_func[arg][1]}' for arg in arg_to_func]
+        args_string = '\n'.join(string_items)
+        print(f'Accepted args:\n{args_string}')
+
     for arg in sys.argv[1:]:
-        arg_to_func[arg]()
+        if arg in arg_to_func:
+            arg_to_func[arg][0]()
 
 
 if __name__ == "__main__":
